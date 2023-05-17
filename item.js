48f1cc1120e9034b80ab0e3d62f90a528405ae28
jolyvoly <79007322+jolyvoly@users.noreply.github.com>
2023-05-17 12:20:08 +0900

    Add files via upload

---
+++ item.js
@@ -0,0 +1,31 @@
+var Ptag = '<p class="popup">',
+Itag = '<span class="item">',
+Name = [
+'독버섯',
+'양주',
+'광대꽃의 눈',
+'식물덩어리',
+'두리안',
+'야매약',
+'나무수액',
+'설탕수정'
+],
+
+Info = [
+'허기 +1, 정신 -1',
+'마실 경우 체력 -1, 정신 +1<br>상처에 사용할 경우 체력 +1, 정신 -1',
+'허기 +1, 체력 +1, 정신 -2',
+'허기 +1, 정신 -1',
+'허기 +1, 정신 -1',
+'체력 +2, 정신 -2',
+'체력 +1',
+'허기 +1'
+];
+
+for(i=0;i<Name.length;i++){
+document.getElementById("item"+i).innerHTML = Itag + Name[i] + Ptag + Info[i] +"</p></span>";
+}
+
+
+
+
