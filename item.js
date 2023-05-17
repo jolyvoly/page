b9812ad0d99ae0b907ad38dc985971b35f758447
jolyvoly <79007322+jolyvoly@users.noreply.github.com>
2023-05-17 14:57:37 +0900

    Update item.js

--- item.js
+++ item.js
@@ -4,8 +4,10 @@ Name = ['독버섯','양주','광대꽃의 눈','식물덩어리','두리안','�
 
 Info = ['허기 +1, 정신 -1','마실 경우 체력 -1, 정신 +1<br>상처에 사용할 경우 체력 +1, 정신 -1','허기 +1, 체력 +1, 정신 -2','허기 +1, 정신 -1','허기 +1, 정신 -1','체력 +2, 정신 -2','체력 +1','허기 +1'];
 
-for(i=0;i<Name.length;i++){
-document.getElementById("item"+i).innerHTML = Itag + Name[i] + Ptag + Info[i] +"</p></span>";
+for (let i = 0; i < Name.length; i++) 
+ {let element = document.getElementById("item" + i);
+if (element) {
+    element.innerHTML = Itag + Name[i] + Ptag + Info[i] + "</p></span>";
+ }
 }
-
 
